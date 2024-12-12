5a31ff98f79d22ceaafc29a33e2ed97377db0670
kaileshwar16 <kaileshwar2005@gmail.com>
2024-12-12 19:09:09 +0530

    changed color for icons and created a component of header

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header =({title}) =>
+{
+    return (
+        <header className=" flex w-full justify-center items-center bg-gray-800 h-14 z-10 backdrop-blur-md border-b border-gray-700 tracking-widest">{title}</header>
+    )
+}
+export default Header;
